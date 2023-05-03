db7cd13d34f40290b848537beddd05e28a080215
unknown <mainulisraf163@gmail.com>
2023-05-03 17:37:50 +0600

    first commit

---
+++ json.php
@@ -0,0 +1,23 @@
+<?php
+
+$host="localhost";
+$username="root";
+$password="";
+$database="agent_information";
+
+$connect=mysqli_connect($host, $username, $password, $database);
+
+$sql="SELECT * FROM geo_set_division";
+$results=mysqli_query($connect,$sql);
+$json_array=array();
+
+while($data=mysqli_fetch_assoc($results)){
+
+$json_array[]=$data;
+
+
+
+}
+echo json_encode($json_array)
+
+?>
